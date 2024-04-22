f16cca24cb9dc51c7b9a5ff412d77484031253ae
minzx23 <144357990+minzx23@users.noreply.github.com>
2024-04-23 00:47:59 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,7 @@
+def multiplication(n):
+    for i in range(1,10,1):
+        print(f'{n} X {i} = {n*i:2d}\t{n+1} X {i} = {(n+1)*i:2d}\t{n+2} X {i} = {(n+2)*i:2d}\t{n+3} X {i} = {(n+3)*i:2d}')
+
+multiplication(2)
+print()
+multiplication(6)
